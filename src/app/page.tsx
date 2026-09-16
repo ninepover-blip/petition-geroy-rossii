aa33e9e74071cc23305923a5f68f3e38bb661cc9
IT-LayF <layf@github.com>
2026-09-16 19:00:42 +0300

    fix: remove Story, Ticker, StatsStrip, Reasons, Updates, SharePanel from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import Ticker, { WordStrip } from "@/components/Ticker";
-import StatsStrip from "@/components/StatsStrip";
-import Story from "@/components/Story";
-import Reasons from "@/components/Reasons";
-import Updates from "@/components/Updates";
 import SignForm from "@/components/SignForm";
-import SharePanel from "@/components/SharePanel";
 import Footer, { MobileCta } from "@/components/Footer";
 import { LiveProvider } from "@/components/live";
 import { getPetitionStats } from "@/lib/data";
@@ -21,14 +15,7 @@ export default async function Home() {
       <Header />
       <main>
         <Hero />
-        <Ticker />
-        <StatsStrip />
-        <Story />
-        <WordStrip />
-        <Reasons />
         <SignForm />
-        <Updates />
-        <SharePanel />
       </main>
       <Footer />
       <MobileCta />
